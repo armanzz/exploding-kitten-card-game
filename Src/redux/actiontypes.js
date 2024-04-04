4873f2f8f118fbb69b6fae8dabb51f6a9098ab77
armanzz <armansingh1132@gmail.com>
2024-04-04 05:43:59 -0700

    first commit

--- Src/redux/actiontypes.js
+++
@@ -1,9 +0,0 @@
-// actionTypes.js
-
-export const DRAW_CARD = 'DRAW_CARD';
-export const START_GAME = 'START_GAME';
-export const SET_USERNAME = 'SET_USERNAME';
-
-
-export const WIN_GAME = 'WIN_GAME';
-export const LOSE_GAME = 'LOSE_GAME';
